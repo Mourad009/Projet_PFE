b6673875a9ce9dc5f999433b5c38b9151ac62c88
LENOVO X240 <=mouradhmednellah@gmail.com>
2024-05-05 16:46:06 +0000

    test etape 5

--- database/migrations/2024_05_05_154120_create_postes_table.php
+++ database/migrations/2024_05_05_154120_create_postes_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('body');
             $table->string('files')->nullable();
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
